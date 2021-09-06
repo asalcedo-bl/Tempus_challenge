0f6b8c7b487daa7e9c2b9ca49a5c123df61eed32
asalcedo31 <adrisal31@gmail.com>
2021-09-06 15:05:38 -0400

    added functions to collapse multiple alleles into a single character for each varant and to generate a basic annotation data frame

--- helper_functions.R
+++ helper_functions.R
@@ -1,6 +1,6 @@
 
 process_inputs <- function(){
-	# reads a vcf from a system file and outputs a list with a VCF object and a character vector of the outputfile
+	# reads a vcf from a system file and outputs a list with a VCF object, a character vector of the outputfile, and a list of samples to analyze
 	# vcf_file is a character vertor length (1) with the vcf file to read
 	# output_file is a character vertor length (1) with the name of the output annotation file
 	# genome is the genome version
@@ -17,7 +17,7 @@ process_inputs <- function(){
 					readVcf(file=args$vcf_file, genome=args$genome)
 				},
 				error =  function(cond) {
-						    		print("unable to read VCF file")
+						    		print("Unable to read VCF file")
 						    		msg <- conditionMessage(cond)
 						            stop(msg)         
 									return(NULL)
@@ -31,17 +31,34 @@ process_inputs <- function(){
 		output_file <- args$output_file
 	}
 	
-	# gather samples to output
+	# gather samples to output	
 	all_samples <- samples(header(vcf))
 	if( 'all' == args$samples ){
 		samples <- all_samples
 	}else{
 	# check that all of the requested samples are in the vcf
-		a_ply(args$samples, 1, function(x) if(!(x %in% all_samples)) print( paste(x, "not found in vcf file")))
+		a_ply(args$samples, 1, function(x) if(!(x %in% all_samples)) warning( paste(x, "not found in vcf file")))
 		samples <- all_samples[all_samples %in% args$samples]
+	}
+
+	if(length(samples) == 0){
+		stop("No samples specified. Specify valid sample names for which to annotate variants or 'all' for to output annotation for all samples")
 	}
 
 	return(list(vcf=vcf, output_file=output_file, samples=samples))
 }
 
+collapse_alleles <- function(x, separator=","){
+	# collapses a vector of alleles and outputs a character vector of length(1) with all alleles separated by a user defined separator
+	# x is a character vector of alleles
+	# separator is a character vector of length 1 with the character to be used to separate alternative alleles
+	collapsed_alleles <- ifelse(length(x) == 1, as.character(x), paste(x, collapse=separator))
+
+	return(collapsed_alleles)
+}
+
+annotate_variants <- function(vcf, samples){
+	alt_alleles <- laply(alt(vcf), collapse_alleles)
+	annotation <- data.frame(chr=as.character(seqnames(vcf)), start=start(vcf), id=rownames(info(vcf)), ref=as.character(ref(vcf)), alt=alt_alleles)
+}
 
--- main.R
+++ main.R
@@ -4,7 +4,7 @@ library(data.table)
 library(stringr)
 library(argparse)
 library(stringr)
-
+library(plyr)
 
 source("helper_functions.R")
 
@@ -13,6 +13,8 @@ tmp <-  process_inputs()
 vcf <- tmp[[1]]
 output_file <- tmp[[2]]
 samples <- tmp[[3]]
+
+
 
 
 
